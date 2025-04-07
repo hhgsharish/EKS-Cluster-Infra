62d6e5bafa9242ad2724d2de993094603b025006
Harisha G <hhgsharish@gmail.com>
2025-04-07 10:20:05 +0530

    Update main.tf - Added 22 SSH Port for Master Node

--- main.tf
+++ main.tf
@@ -143,6 +143,13 @@ resource "aws_security_group" "k8s_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+ ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     description = "Allow HTTPS"
